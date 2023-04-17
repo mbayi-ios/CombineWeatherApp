856c6b593237c1ffe1ed105a48cb7db7c3cad81c
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-04-17 17:48:33 +0300

    implementing weatherapp using combine and swiftui

--- CombinedWeatherApp/Weather/OpenWeatherAPI/WeatherFetcher.swift
+++ CombinedWeatherApp/Weather/OpenWeatherAPI/WeatherFetcher.swift
@@ -1,33 +1,12 @@
-/// Copyright (c) 2019 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
 
 import Foundation
 import Combine
+
+protocol WeatherFetchable {
+  func weeklyWeatherForecast(forCity city: String) -> AnyPublisher<WeeklyForecastResponse, WeatherError>
+
+  func currentWeatherForecast(forCity city: String) -> AnyPublisher<CurrentWeatherForecastResponse, WeatherError>
+}
 
 class WeatherFetcher {
   private let session: URLSession
@@ -37,18 +16,48 @@ class WeatherFetcher {
   }
 }
 
+extension WeatherFetcher: WeatherFetchable {
+  func weeklyWeatherForecast(forCity city: String) -> AnyPublisher<WeeklyForecastResponse, WeatherError> {
+    return forecast(with: makeWeeklyForecastComponents(withCity: city))
+  }
+
+  func currentWeatherForecast(forCity city: String) -> AnyPublisher<CurrentWeatherForecastResponse, WeatherError> {
+    return forecast(with: makeCurrentDayForecastComponents(withCity: city))
+  }
+
+  private func forecast<T>(with components: URLComponents) -> AnyPublisher<T, WeatherError> where T: Decodable {
+    //1
+    guard let url = components.url else {
+      let error = WeatherError.network(description: "Couldnt create url")
+      return Fail(error: error).eraseToAnyPublisher()
+    }
+    //2
+    return session.dataTaskPublisher(for: URLRequest(url: url))
+    //3
+      .mapError { error in
+      .network(description: error.localizedDescription)
+      }
+    //4
+      .flatMap(maxPublishers: .max(1)) { pair in
+        decode(pair.data)
+      }
+    //5
+      .eraseToAnyPublisher()
+  }
+
+
+}
+
 // MARK: - OpenWeatherMap API
 private extension WeatherFetcher {
   struct OpenWeatherAPI {
     static let scheme = "https"
     static let host = "api.openweathermap.org"
     static let path = "/data/2.5"
-    static let key = "<your key>"
+    static let key = "552c4d730c6fa42329c1f355129dae3e"
   }
   
-  func makeWeeklyForecastComponents(
-    withCity city: String
-  ) -> URLComponents {
+  func makeWeeklyForecastComponents(withCity city: String) -> URLComponents {
     var components = URLComponents()
     components.scheme = OpenWeatherAPI.scheme
     components.host = OpenWeatherAPI.host
@@ -64,9 +73,7 @@ private extension WeatherFetcher {
     return components
   }
   
-  func makeCurrentDayForecastComponents(
-    withCity city: String
-  ) -> URLComponents {
+  func makeCurrentDayForecastComponents(withCity city: String) -> URLComponents {
     var components = URLComponents()
     components.scheme = OpenWeatherAPI.scheme
     components.host = OpenWeatherAPI.host
